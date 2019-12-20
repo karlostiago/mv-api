4ac826916797571429c81cdf39e17fadf51b6ba6
Carlos Tiago <carlostiagodesousa@gmail.com>
2019-12-19 22:12:59 -0300

    primeiro commit

---
+++ src/main/java/com/mv/api/MvApiApplication.java
@@ -0,0 +1,13 @@
+package com.mv.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mv/api/MvApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mv.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
